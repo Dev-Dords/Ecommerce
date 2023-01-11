e1425ce773dc65b21820bcca06820cd350e57551
Dev-Dords <jrenzodorado@icloud.com>
2023-01-11 14:58:02 +0800

    Added Data Parsing - Arrays and Images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+              <div className="product-info">
+                <p> {product.name}</p>
+                <p> <strong>${product.price}</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
